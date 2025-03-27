37ab2e2ba6812d2ee86e806970fe16d5b2e354fb
Konstantin Burlachenko <burlachenkok@gmail.com>
2025-03-27 11:30:31 +0000

    up

--- experiments/exp1_tiny_example/torch_cpp/build_mac.sh
+++ experiments/exp1_tiny_example/torch_cpp/build_mac.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-LIBTORCH_PATH="/Users/XXX/Downloads"
+#https://download.pytorch.org/libtorch/cpu/libtorch-macos-x86_64-2.2.2.zip
+LIBTORCH_PATH="/Users/burlack/Downloads/libtorch22"
 
 g++ -x c++ --std=c++17 \
--D_GLIBCXX_USE_CXX11_ABI=1 \
--I${LIBTORCH_PATH}/libtorch/include \
--I${LIBTORCH_PATH}/libtorch/include/torch/csrc/api/include \
--L${LIBTORCH_PATH}/libtorch/lib \
+-D_GLIBCXX_USE_CXX11_ABI=0 \
+-I${LIBTORCH_PATH}/include \
+-I${LIBTORCH_PATH}/include/torch/csrc/api/include \
+-L${LIBTORCH_PATH}/lib \
 -O3 -flto \
 main.cpp \
--o test_app \
+-o test_app_macos \
 -ltorch -lc10 -ltorch_cpu -lasmjit -lcpuinfo -ldnnl -lfbgemm -lfmt -lkineto -lprotobuf -lprotoc -lpthreadpool -lXNNPACK
 
-# export DYLD_LIBRARY_PATH=${LIBTORCH_PATH}/libtorch/lib
+export DYLD_LIBRARY_PATH=${LIBTORCH_PATH}/lib
+./test_app_macos
